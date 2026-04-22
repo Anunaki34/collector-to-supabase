ba6947bc82eb09f42df3125724ce4accc7ba4047
Anunaki34 <112365110+Anunaki34@users.noreply.github.com>
2026-04-22 13:18:42 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export SUPABASE_HOST='lgqqvsbpoycdqzljhptu.supabase.co'
+export SUPABASE_SERVICE_ROLE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxncXF2c2Jwb3ljZHF6bGpocHR1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NjY2ODc0MywiZXhwIjoyMDkyMjQ0NzQzfQ.5bJ00rpGIA_Uo3ks4zAafThW72FhQTzvbZ1erimt0vE'
+export SYMBOL='btcusdt'
+
+npm install
+npm start
